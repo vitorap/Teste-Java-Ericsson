4ef6eecf680dd1209d0860c9d95dc1dcb38e4b4f
Vitor AP <vitor.ap@protonmail.com>
2021-01-20 09:18:07 -0300

    Hello World
    # Quick Description
    Criaçao de um Hello World
    
    # ## Tags ##
    #
    # [x]  Add = Create a capability e.g. feature, test, dependency.
    # [ ]  Drop = Delete a capability e.g. feature, test, dependency.
    # [ ]  Fix = Fix an issue e.g. bug, typo, accident, misstatement.
    # [ ]  Bump = Increase the version of something e.g. a dependency.
    # [ ]  Make = Change the build process, or tools, or infrastructure.
    # [ ]  Start = Begin doing something; e.g. enable a toggle, feature flag, etc.
    # [ ]  Stop = End doing something; e.g. disable a toggle, feature flag, etc.
    # [ ]  Optimize = A change that MUST be about performance, e.g. speed up code.
    # [ ]  Document = A change that MUST be in the documentation, e.g. help files.
    # [ ]  Refactor = A change that MUST be refactoring.
    # [ ]  Reformat = A change that MUST be format, e.g. indent line, trim space, etc.
    # [ ]  Rephrase = A change that MUST be textual, e.g. edit a comment, doc, etc.
    #
    #
    #
    # ## If there is something difference to reproduce, follow this steps ##
    #
    #
    #
    #
    # ## Additional Information? ##
    #
    #
    #
    #
    #

---
+++ src/main/java/ap/vitor/testeEricsson/TesteEricssonApplication.java
@@ -0,0 +1,13 @@
+package ap.vitor.testeEricsson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteEricssonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteEricssonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ap/vitor/testeEricsson/TesteEricssonApplicationTests.java
@@ -0,0 +1,13 @@
+package ap.vitor.testeEricsson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteEricssonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
